8a66535a4caa86b35e328255822d8e9ac53e0c30
elias-asd8 <eliasbrrbrr@gmail.com>
2026-03-08 08:23:45 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,13 +3,14 @@ pkg upgrade -y && pkg install pd
 pd install Ubuntu 
 pd login ubuntu
 apt install aria2
+apt install tigervnc
 aria2c -x 16 -s 16 "https://releases.ubuntu.com/24.04.4/ubuntu-24.04.4-desktop-amd64.iso" -o ubuntu.iso
 qemu-img create -f qcow2 ubuntu.qcow2 70G
 qemu-system-x86_64 \
   -m 4096 \
     -cpu max \
       -smp 4 \
-        -hda ~/ubuntu.qcow2 \
+        -hda ~/ubuntu.qcow2 \q
           -cdrom ~/ubuntu.iso \
             -boot d \
               -vga virtio \
